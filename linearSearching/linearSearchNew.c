5a46a039640cb0db8d381b847cbee4051d5e9c88
Leander Lubguban <Lorenzolubguban@gmail.com>
2023-04-28 07:54:14 +0800

    some other stuff

--- linearSearching/linearSearchNew.c
+++ linearSearching/linearSearchNew.c
@@ -2,11 +2,11 @@
 
 int linearSearch(int *n, int l, int x){
     for(int i = 0; i < l; i++)
-        if(x == n[i]) return i + 1;
+        if(x == n[i]) return i;
     return -1;
 }
 
 int main(){
     int n[] = {1, 2, 3, 4, 5};
-    printf("%d",linearSearch(n, sizeof(n)/sizeof(int)-1, 3));
+    printf("%d",linearSearch(n, sizeof(n)/sizeof(int), 5));
 }
